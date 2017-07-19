6ad53cc6d8f91def10a76378768c346aa1695afe
Steve Jones <sjones@sqlservercentral.com>
2017-07-19 15:16:37 -0600

    Minor updates to the PPT to reorganize for this talk. Updated code to match the demos in the PPT

---
+++ SQL/11_TestData.sql
@@ -0,0 +1,215 @@
+/*
+Unit Testing T-SQL - Checking Joins
+
+Steve Jones, copyright 2017
+
+This code is provided as is for demonstration purposes. It may not be suitable for
+your environment. Please test this on your own systems. This code may not be republished 
+or redistributed by anyone without permission.
+You are free to use this code inside of your own organization.
+*/
+USE TestingTSQL;
+GO
+
+-- We want to ensure we have a known set of test data
+-- Set a table to 1 to reload
+DECLARE
+    @SalesTax BIT = 0,
+    @SalesOrderDetail BIT = 1;
+
+
+
+
+
+
+
+IF @SalesOrderDetail = 1
+    TRUNCATE TABLE dbo.SalesOrderDetail;
+IF @SalesTax = 1
+    TRUNCATE TABLE dbo.Salestax;
+-- ...
+IF @SalesOrderDetail = 1
+    INSERT INTO dbo.SalesOrderDetail
+    (   SalesOrderID,
+        SalesOrderDetailID,
+        OrderQuantity,
+        ProductID,
+        UnitPrice,
+        DiscountPercent,
+        LineTotal,
+        TaxAmount,
+        ShippingState
+    )
+    VALUES
+    (
+        1, 1, 10, 2, 10, 0.0, 100, 2, 'PA'
+    ),
+    (
+        1, 2, 22, 3, 5, 0.1, 100, 5, 'GA'
+    ),
+    (
+        2, 3, 5, 2, 4, 0.15, 17, 0.85, 'GA'
+    ),
+    (
+        2, 4, 12, 3, 10, 0.1, 108, 2.268, 'CO'
+    ),
+    (
+        2, 5, 5, 4, 60, 0.0, 300, 18.60, 'CA'
+    );
+IF @SalesTax = 1
+    INSERT dbo.Salestax
+    (   statecode,
+        taxamount
+    )
+    VALUES
+    (
+        'AK', 0.0714
+    ),
+    (
+        'AL', 0.0214
+    ),
+    (
+        'AR', 0.034
+    ),
+    (
+        'AZ', 0.011
+    ),
+    (
+        'CA', 0.062
+    ),
+    (
+        'CO', 0.021
+    ),
+    (
+        'CT', 0.064
+    ),
+    (
+        'DE', 0.032
+    ),
+    (
+        'FL', 0.06
+    ),
+    (
+        'GA', 0.05
+    ),
+    (
+        'HI', 0.08
+    ),
+    (
+        'IA', 0.044
+    ),
+    (
+        'ID', 0.031
+    ),
+    (
+        'IL', 0.074
+    ),
+    (
+        'IN', 0.071
+    ),
+    (
+        'KS', 0.074
+    ),
+    (
+        'KY', 0.074
+    ),
+    (
+        'LA', 0.071
+    ),
+    (
+        'MA', 0.071
+    ),
+    (
+        'MD', 0.071
+    ),
+    (
+        'ME', 0.074
+    ),
+    (
+        'MI', 0.0714
+    ),
+    (
+        'MN', 0.0714
+    ),
+    (
+        'MO', 0.0714
+    ),
+    (
+        'MS', 0.0714
+    ),
+    (
+        'MT', 0.0714
+    ),
+    (
+        'NC', 0.0714
+    ),
+    (
+        'ND', 0.0714
+    ),
+    (
+        'NE', 0.0714
+    ),
+    (
+        'NH', 0.0714
+    ),
+    (
+        'NJ', 0.0714
+    ),
+    (
+        'NM', 0.0714
+    ),
+    (
+        'NV', 0.0714
+    ),
+    (
+        'NY', 0.0714
+    ),
+    (
+        'OH', 0.0714
+    ),
+    (
+        'OK', 0.0714
+    ),
+    (
+        'OR', 0.0714
+    ),
+    (
+        'PA', 0.02
+    ),
+    (
+        'RI', 0.0714
+    ),
+    (
+        'SC', 0.0714
+    ),
+    (
+        'SD', 0.0714
+    ),
+    (
+        'TN', 0.0714
+    ),
+    (
+        'TX', 0.0714
+    ),
+    (
+        'UT', 0.0714
+    ),
+    (
+        'VA', 0.0714
+    ),
+    (
+        'VT', 0.074
+    ),
+    (
+        'WA', 0.071
+    ),
+    (
+        'WI', 0.024
+    ),
+    (
+        'WV', 0.014
+    ),
+    (
+        'WY', 0.014
+    );
+GO
---
+++ SQL/31_checking_joins.sql
@@ -0,0 +1,276 @@
+/*
+Unit Testing T-SQL - Checking Joins
+
+Steve Jones, copyright 2016
+
+This code is provided as is for demonstration purposes. It may not be suitable for
+your environment. Please test this on your own systems. This code may not be republished 
+or redistributed by anyone without permission.
+You are free to use this code inside of your own organization.
+*/
+
+-- We want to alter a table.
+USE [TestingTSQL]
+GO
+
+-- we have a procedure using a join
+-- DROP PROCEDURE dbo.GetArticleHeadlines
+CREATE PROCEDURE dbo.GetArticleHeadlines
+AS
+BEGIN
+SELECT TOP 5
+    ci.ContentItemID
+   ,ci.Title
+   ,ci.ExternalURL
+  , cpr.AverageRating
+   ,cpr.ViewsLastNDays
+   ,cpr.TotalViews
+  FROM
+    dbo.ContentItems AS ci
+  INNER JOIN dbo.ContentPerformanceRecord AS cpr
+  ON
+    cpr.ContentItemID = ci.ContentItemID; 
+END
+go
+
+
+
+
+
+-- this returns current headlines.
+-- build a test
+EXEC tsqlt.NewTestClass
+  @ClassName = N'tContentTests';
+GO
+CREATE PROCEDURE [tContentTests].[test GetArticleHeadlines for headlines and ratings, and views]
+AS
+BEGIN
+-- assemble
+EXEC tsqlt.FakeTable
+  @TableName = N'ContentItems'
+, @SchemaName = N'dbo';
+INSERT dbo.ContentItems ( ContentItemID, Title, ExternalURL)
+ VALUES (1, 'Test 1', 'http://someurl.com/1/')
+      , (2, 'Test 2', 'http://someurl.com/2/')
+      , (3, 'Test 3', 'http://someurl.com/3/')
+      , (4, 'Test 4', 'http://someurl.com/4/')
+      , (5, 'Test 5', 'http://someurl.com/5/')
+      , (6, 'Test 6', 'http://someurl.com/6/')
+      , (7, 'Test 7', 'http://someurl.com/7/')
+      , (8, 'Test 8', 'http://someurl.com/8/')
+      , (9, 'Test 9', 'http://someurl.com/9/')
+      , (10, 'Test 10', 'http://someurl.com/10/')
+      , (11, 'Test 11', 'http://someurl.com/11/');
+
+EXEC tsqlt.FakeTable
+  @TableName = N'ContentPerformanceRecord'
+, @SchemaName = N'dbo';
+
+INSERT dbo.ContentPerformanceRecord (ContentItemID, AverageRating, ViewsLastNDays, TotalViews)
+ VALUES
+ (1, 4.0, 10, 20 )
+,(2, 5.2, 20, 40 )
+,(3, 6.5, 40, 80 )
+
+CREATE TABLE #expected 
+( ContentItemID INT
+, Title VARCHAR(200)
+, ExternalURL VARCHAR(250)
+, AverageRating NUMERIC(18, 4)
+, ViewsLastNDays INT
+, TotalViews int
+)
+INSERT #expected
+ VALUES
+ ( 1, 'Test 1', 'http://someurl.com/1/', 4.0, 10, 20 )
+,( 2, 'Test 2', 'http://someurl.com/2/', 5.2, 20, 40 )
+,( 3, 'Test 3', 'http://someurl.com/3/', 6.5, 40, 80 )
+
+SELECT *
+ INTO #actual
+  FROM #expected AS e
+  WHERE 1 = 0
+
+-- act
+INSERT #actual 
+EXEC dbo.GetArticleHeadlines;
+
+-- assert
+EXEC tsqlt.AssertEqualsTable
+  @Expected = N'#expected'
+, @Actual = N'#actual'
+, @FailMsg = N'Incorrect headlines returned';
+
+END
+GO
+
+
+
+
+
+
+-- test
+EXEC tsqlt.run '[tContentTests].[test GetArticleHeadlines for headlines and ratings, and views]';
+GO
+
+
+
+
+
+
+
+
+
+
+-- however, not all content items have ratings
+-- if needed: DELETE dbo.ContentPerformanceRecord WHERE ContentItemID > 10
+SELECT
+    ci.ContentItemID
+  , cpr.ContentItemID
+  FROM
+    dbo.ContentItems AS ci
+  LEFT OUTER JOIN dbo.ContentPerformanceRecord AS cpr
+  ON
+    cpr.ContentItemID = ci.ContentItemID;
+
+GO
+
+
+
+
+
+-- let's change the procedure
+-- Add LOJ and ISNULL items
+ALTER PROCEDURE dbo.GetArticleHeadlines
+AS
+BEGIN
+SELECT TOP 5
+    ci.ContentItemID
+   ,ci.Title
+   ,ci.ExternalURL
+  , AverageRating = ISNULL(cpr.AverageRating,'0')
+   ,ViewsLastNDays = ISNULL(cpr.ViewsLastNDays,'0')
+   ,TotalViews =ISNULL(cpr.TotalViews,'0')
+  FROM
+    dbo.ContentItems AS ci
+  LEFT OUTER JOIN dbo.ContentPerformanceRecord AS cpr
+  ON
+    cpr.ContentItemID = ci.ContentItemID; 
+END
+go
+
+
+-- check
+EXEC dbo.GetArticleHeadlines;
+go
+
+
+
+
+-- seems to work
+-- let's test
+-- test
+EXEC tsqlt.run '[tContentTests].[test GetArticleHeadlines for headlines and ratings, and views]';
+GO
+
+
+
+
+
+
+
+
+-- failure. We can see that changing joins is an issue.
+
+
+
+
+
+
+
+
+-- alter the test
+-- all we need to change is our Expected table
+ALTER PROCEDURE [tContentTests].[test GetArticleHeadlines for headlines and ratings, and views]
+AS
+BEGIN
+-- assemble
+EXEC tsqlt.FakeTable
+  @TableName = N'ContentItems'
+, @SchemaName = N'dbo';
+INSERT dbo.ContentItems ( ContentItemID, Title, ExternalURL)
+ VALUES (1, 'Test 1', 'http://someurl.com/1/')
+      , (2, 'Test 2', 'http://someurl.com/2/')
+      , (3, 'Test 3', 'http://someurl.com/3/')
+      , (4, 'Test 4', 'http://someurl.com/4/')
+      , (5, 'Test 5', 'http://someurl.com/5/')
+      , (6, 'Test 6', 'http://someurl.com/6/')
+      , (7, 'Test 7', 'http://someurl.com/7/')
+      , (8, 'Test 8', 'http://someurl.com/8/')
+      , (9, 'Test 9', 'http://someurl.com/9/')
+      , (10, 'Test 10', 'http://someurl.com/10/')
+      , (11, 'Test 11', 'http://someurl.com/11/');
+
+EXEC tsqlt.FakeTable
+  @TableName = N'ContentPerformanceRecord'
+, @SchemaName = N'dbo';
+
+INSERT dbo.ContentPerformanceRecord (ContentItemID, AverageRating, ViewsLastNDays, TotalViews)
+ VALUES
+ (1, 4.0, 10, 20 )
+,(2, 5.2, 20, 40 )
+,(3, 6.5, 40, 80 )
+
+CREATE TABLE #expected 
+( ContentItemID INT
+, Title VARCHAR(200)
+, ExternalURL VARCHAR(250)
+, AverageRating NUMERIC(18, 4)
+, ViewsLastNDays INT
+, TotalViews int
+)
+INSERT #expected
+ VALUES
+ ( 1, 'Test 1', 'http://someurl.com/1/', 4.0, 10, 20 )
+,( 2, 'Test 2', 'http://someurl.com/2/', 5.2, 20, 40 )
+,( 3, 'Test 3', 'http://someurl.com/3/', 6.5, 40, 80 )
+, (4, 'Test 4', 'http://someurl.com/4/', 0, 0, 0)
+, (5, 'Test 5', 'http://someurl.com/5/', 0, 0, 0)
+
+SELECT *
+ INTO #actual
+  FROM #expected AS e
+  WHERE 1 = 0
+
+-- act
+INSERT #actual 
+EXEC dbo.GetArticleHeadlines;
+
+-- assert
+EXEC tsqlt.AssertEqualsTable
+  @Expected = N'#expected'
+, @Actual = N'#actual'
+, @FailMsg = N'Incorrect headlines returned';
+
+END
+GO
+
+
+
+
+
+
+-- test
+EXEC tsqlt.run '[tContentTests].[test GetArticleHeadlines for headlines and ratings, and views]';
+GO
+
+
+
+
+
+
+/*******************************************************************************
+*                                                                              *
+*                            END DEMO                                          *
+*                                                                              *
+********************************************************************************/
